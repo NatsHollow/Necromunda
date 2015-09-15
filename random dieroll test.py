fa3a5e8623a1a295b0d835301903ac6b5845315e
NatsHollow <jporter1978@Gmail.com>
2015-09-15 12:36:13 -0400

    words go here

---
+++ random dieroll test.py
@@ -0,0 +1,13 @@
+import random
+
+def DieRoll ():
+    Die = random.randint (1,6)
+    return Die
+
+print DieRoll()
+print DieRoll()
+print DieRoll()
+print DieRoll()
+print DieRoll()
+print DieRoll()
+
